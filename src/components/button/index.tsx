39cbfff449fa2440767f9c1ae6ca09ce42c16122
maecha <hiiamseiya@gmail.com>
2021-04-28 18:20:04 +0900

    [update] 記事作成ボタンを付けた

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,13 +6,17 @@ import styles from './index.module.css'
 
 // HTML ほとんどそのままのコンポーネントで、本来の props をそのまま受け取る
 // ref: https://ginpen.com/2019/05/13/componentpropswithref-to-receive-html-props-in-react/
-type Props = React.ComponentPropsWithRef<'button'>
+type Props =
+  | (React.ComponentPropsWithRef<'button'> & { as: undefined })
+  | (React.ComponentPropsWithRef<'button'> & { as: 'button' })
+  | (React.ComponentPropsWithRef<'a'> & { as: 'a' })
 
-export const Button: React.FC<Props> = ({ className, children, ...rest }) => {
+export const Button: React.FC<Props> = ({
+  className,
+  children,
+  as = 'button',
+  ...rest
+}) => {
   const _className = useClassNames(styles.button, className)
-  return (
-    <button className={_className} {...rest}>
-      {children}
-    </button>
-  )
+  return React.createElement(as, { ...rest, className: _className }, children)
 }
